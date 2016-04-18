d97e59abb46a5bf651d53bfc6ecb1ac34a97032a
stephenbienkiewicz <bienkiewicz@outlook.com>
2016-04-18 18:24:02 -0400

    added methods

--- lesson_02_homework.rb
+++ lesson_02_homework.rb
@@ -20,13 +20,14 @@ def full_name(first_name, last_name, title)
   return name
 end
 
+
 # Fix Broken Code
 
 def full_name(f, l)
-  puts f + l
+  f + l
 end
 
-puts full_nam("bubba", "chuck")
+puts full_name("bubba", "chuck")
 
 
 # Coding
@@ -34,13 +35,32 @@ puts full_nam("bubba", "chuck")
 
 ## Enter your code for your 'add' method below
 
+def addnum(firstnum, secondnum)
+  firstnum + secondnum
+end
 
+puts addnum(44, 6)
 
 
 ## Enter the code for you 'join_strings' method below
 
+def join_strings(carmake, carmodel)
+  carmake + carmodel
+end
 
-
+puts join_strings("honda", " civic")
 
 
 ## Enter your 'old_enough_to_vote?' method below
+
+def old_enough_to_vote(year)
+  if year <= 1998
+    'Wow, you are old enough to vote!'
+  else
+    'Come back when you are older!'
+  end
+end
+
+puts old_enough_to_vote(1999)
+
+
