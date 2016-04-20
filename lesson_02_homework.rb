de501da6c65946578c9f574eb202b126d91a56b7
David Noriega <dnori817@gmail.com>
2016-04-19 23:39:39 -0400

    completed assignment

--- lesson_02_homework.rb
+++ lesson_02_homework.rb
@@ -29,32 +29,54 @@ def full_name(first_name, last_name, title)
   end
 
   return name
+  #returns name when method is run
 end
 
 puts full_name('dave', 'noriega', 'mr')
 
 puts full_name(nil, 'noriega', 'mr')
-# Fix Broken Code
 
-# def full_name(first_name, last_name)
-#   puts first_name.capitalize + " " + last_name.capitalize
-# end
+#Fix Broken Code
 
-#puts full_name(nil, nil, nil )
+def full_name(first_name, last_name)
+  puts first_name.capitalize + " " + last_name.capitalize
+  #I added the .capitlize method to capitalize the first letter of the names
+end
+
+puts full_name('bubba', 'chuck')
 
 
 # Coding
 
 
 ## Enter your code for your 'add' method below
+def add(number_1, number_2)
+  number_1 + number_2
+end
 
+result = add(5, 10)
 
+puts result
 
 
 ## Enter the code for you 'join_strings' method below
+def join_strings(string_1, string_2)
+  string_1 + " " + string_2
+end
 
-
-
+puts join_strings("hello", "dolly")
 
 
 ## Enter your 'old_enough_to_vote?' method below
+def old_enough_to_vote(year)
+  if year <= 1998
+    return "yes"
+  else 
+    return "no"  
+  end
+end
+
+puts old_enough_to_vote(1999)
+
+puts old_enough_to_vote(1979)
+
